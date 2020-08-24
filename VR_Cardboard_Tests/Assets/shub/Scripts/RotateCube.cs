55181d88dda2f5c1c27ec13a2e4658b1a240dcd9
Shubham Mandal <shubhammandal96@gmail.com>
2020-08-24 21:19:06 +0530

    VR_Cardboard_Tests

---
+++ VR_Cardboard_Tests/Assets/shub/Scripts/RotateCube.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCube : MonoBehaviour
+{
+  public float spinForce = 40;
+  Vector3 rotateInstruction = new Vector3(0, 1, 0);
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    changeSpin();
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    transform.Rotate(rotateInstruction * spinForce * Time.deltaTime);
+    // Debug.Log("SHUBHAM1" + (transform.rotation).ToEulerAngles());
+    // Debug.Log(Camera.main.transform.forward);
+  }
+
+  float rangeFloat()
+  {
+    return (float)(Random.Range(0, 20) - 10) / 10.0f;
+  }
+
+  Vector3 set_rotateInstruction()
+  {
+    float x = rangeFloat();
+    float y = rotateInstruction[1];
+    float z = rangeFloat();
+
+    return new Vector3(x, y, z);
+  }
+  public void interactWith_GvrReticlePointer()
+  {
+    changeSpin();
+  }
+  public void changeSpin()
+  {
+    Vector3 get_rotateInstruction = set_rotateInstruction();
+    rotateInstruction = get_rotateInstruction;
+    rotateInstruction[1] = -rotateInstruction[1];
+    Debug.Log(rotateInstruction);
+    transform.Rotate(rotateInstruction * spinForce);
+    Debug.Log("SHUBHAM--" + (transform.rotation).ToEulerAngles());
+  }
+
+}
---
+++ VR_Cardboard_Tests/Assets/shub/Scripts/playerWalk.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class playerWalk : MonoBehaviour
+{
+  public int playerSpeed = 15;
+  private bool gyroEnabled;
+  private Gyroscope m_Gyro;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    gyroEnabled = EnableGyro();
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    // GyroscopeValue();
+    Quaternion rotation = m_Gyro.attitude;
+    Vector3 eulerAngle = rotation.ToEulerAngles();
+
+    Debug.Log("MANDAL:------" + eulerAngle[1]);
+
+    if (gyroEnabled && eulerAngle[1] >= 1.8 && eulerAngle[1] <= 2.0)
+    {
+      Debug.Log("FORWARD");
+      transform.position = transform.position - Camera.main.transform.forward * playerSpeed * Time.deltaTime;
+    }
+    else if (gyroEnabled && eulerAngle[1] >= 1.2 && eulerAngle[1] <= 1.3)
+    {
+      Debug.Log("BACK");
+      transform.position = transform.position + Camera.main.transform.forward * playerSpeed * Time.deltaTime;
+    }
+    Debug.Log("SHUB::::::" + transform.position);
+  }
+
+  private bool EnableGyro()
+  {
+    if (SystemInfo.supportsGyroscope)
+    {
+      m_Gyro = Input.gyro;
+      m_Gyro.enabled = true;
+      return true;
+    }
+    return false;
+  }
+
+  void GyroscopeValue()
+  {
+    //Output the rotation rate, attitude and the enabled state of the gyroscope as a Label
+    Debug.Log("Gyro rotation rate " + m_Gyro.rotationRate);
+    Debug.Log("Gyro attitude" + m_Gyro.attitude);
+    Debug.Log("Gyro enabled : " + m_Gyro.enabled);
+  }
+
+}
